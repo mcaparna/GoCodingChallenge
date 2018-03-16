50cfbdc5c96e33fd1dee2b2af15f074267355932
Aparna <aparna_chandrashekar@homedepot.com>
2018-03-16 13:11:22 -0500

    Testcase updation

--- todo/handlers_test.go
+++ todo/handlers_test.go
@@ -1,23 +1,49 @@
 package todo
 
 import (
-    "fmt"
     "net/http"
     "net/http/httptest"
     "strings"
     "testing"
 	"github.com/mcaparna/GoCodingChallenge/todo"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
-var (
-    server   *httptest.Server
-    usersUrl string
-)
 
-func init() {
-    server = httptest.NewServer(todo.Create()) //Creating new server with the user handlers
+func TestUpdateTodo(t *testing.T) {
+    userJson := `{"status": "Closed", "title": "Clean Home"}`
 
-    usersUrl = fmt.Sprintf("%s/users", server.URL) //Grab the address for the API endpoint
+    reader := strings.NewReader(userJson) //Convert string to reader
+
+    request, err := http.NewRequest("PUT", "http://127.0.0.1:8080/todos/1", reader) //Create request with JSON body
+	
+	res := httptest.NewRecorder()
+
+    if err != nil {
+        t.Error(err) //Something is wrong while sending request
+    }
+	
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	mock.ExpectBegin()
+	mock.ExpectQuery("^SELECT (.+) FROM todo WHERE").WithArgs("id","title","status").WillReturnRows(sqlmock.NewRows([]string{"id","title", "status"}).AddRow("1","val1", "val2"))
+	
+	mock.ExpectPrepare("UPDATE todo SET title = $2, status = $3 WHERE id = $1").ExpectExec().WithArgs("id","title","status").WillReturnResult(sqlmock.NewResult(1, 1))
+	
+	mock.ExpectClose()
+	
+	todo.Update(res, request, nil)
+	
+	if err != nil {
+		t.Errorf("Expected no error, but got %s instead", err)
+	}
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 }
 
 func TestCreateTodo(t *testing.T) {
@@ -25,15 +51,69 @@ func TestCreateTodo(t *testing.T) {
 
     reader := strings.NewReader(userJson) //Convert string to reader
 
-    request, err := http.NewRequest("POST", usersUrl, reader) //Create request with JSON body
-
-    res, err := http.DefaultClient.Do(request)
+    request, err := http.NewRequest("POST", "http://127.0.0.1:8080/todos", reader) //Create request with JSON body
+	
+	res := httptest.NewRecorder()
 
     if err != nil {
         t.Error(err) //Something is wrong while sending request
     }
+	
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	mock.ExpectBegin()
+	mock.ExpectQuery("^SELECT (.+) FROM todo WHERE").WithArgs("id","title","status").WillReturnRows(sqlmock.NewRows([]string{"id","title", "status"}).AddRow("1","val1", "val2"))
+	
+	mock.ExpectPrepare("INSERT into todo").ExpectExec().WithArgs("id","title","status").WillReturnResult(sqlmock.NewResult(1, 1))
+	
+	mock.ExpectClose()
+	
+	todo.Create(res, request, nil)
+	
+	if err != nil {
+		t.Errorf("Expected no error, but got %s instead", err)
+	}
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 
-    if res.StatusCode != 200 {
-        t.Errorf("Success expected: %d", res.StatusCode) //Uh-oh this means our test failed
+}
+
+func TestListTodo(t *testing.T) {
+    userJson := `{"status": "Closed", "title": "Clean Home"}`
+
+    reader := strings.NewReader(userJson) //Convert string to reader
+
+    request, err := http.NewRequest("GET", "http://127.0.0.1:8080/todos", reader) //Create request with JSON body
+	
+	res := httptest.NewRecorder()
+
+    if err != nil {
+        t.Error(err) //Something is wrong while sending request
     }
+	
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Errorf("An error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	mock.ExpectBegin()
+	mock.ExpectQuery("^SELECT (.+) FROM todo WHERE").WithArgs("id","title","status").WillReturnRows(sqlmock.NewRows([]string{"id","title", "status"}).AddRow("1","val1", "val2"))
+	
+	mock.ExpectClose()
+	
+	todo.List(res, request, nil)
+	
+	if err != nil {
+		t.Errorf("Expected no error, but got %s instead", err)
+	}
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
 }
